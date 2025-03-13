2b7a5c8f6d0d62020c7c81e208e10341bed60bae
Nicolashtt <nicolasrianalvesalmeida@gmail.com>
2025-03-13 12:20:23 -0300

    projeto do livro

---
+++ ddd-livraria/src/Livro.java
@@ -0,0 +1,13 @@
+public class Livro {
+
+
+    String titulo;
+    String autor;
+    double preco;
+    String editora;
+    int paginas;
+    String resumo;
+    String tipoCapa;
+
+}
+
---
+++ ddd-livraria/src/TesteLivro.java
@@ -0,0 +1,18 @@
+public class TesteLivro {
+    public static void main(String[] args) {
+
+//criar um objeto -> instanciar um objeto
+        //Sintaxe: Tipo nomeObjeto = new Tipo ();
+        Livro  meuLivro = new Livro();
+        meuLivro.titulo = "One piece 102";
+        meuLivro.autor = "Oda sensei";
+        meuLivro.editora = "shueishia";
+        meuLivro.preco = "31";
+        meuLivro.paginas = "100";
+        meuLivro.resumo = "um garoto que quer ser o rei dos piratas";
+        Livro livroDaClara = new Livro();
+
+    }
+
+}
+
